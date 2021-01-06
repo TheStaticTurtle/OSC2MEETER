cd6335766ca634383958a5bf2e755ce9ab79a1a0
Samuel <tuglersamuel@gmail.com>
2021-01-07 00:07:29 +0100

    Moved thes osc messages to the proper place (and using previous commit values) + Added label to osc messages

--- OSC2MEETER/Program.cs
+++ OSC2MEETER/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpOSC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -15,6 +16,10 @@ namespace OSC2MEETER {
 
         static VoicemeeterRemote.Remote remote = new VoicemeeterRemote.Remote();
 
+        static string stringOR(string a, string b) {
+            return a.Length > 0 ? a : b;
+        }
+
         protected static void myHandler(object sender, ConsoleCancelEventArgs args) {
             remote.Logout();
 
@@ -26,22 +31,60 @@ namespace OSC2MEETER {
             Console.CancelKeyPress += new ConsoleCancelEventHandler(myHandler);
             remote.Login();
 
-            Console.WriteLine("Successfully connect to " + remote.GetVMType() + " version: " + remote.GetVMVersion());
-            Console.WriteLine(remote.GetParameterFloat("Strip[0].gain"));
-            Console.WriteLine(remote.GetParameterStringASCII("Strip[0].Label"));
 
-            remote.SetParameterFloat("Bus[0].gain", 0);
-            remote.SetParameterStringASCII("Strip[1].Label", "TEST");
-            remote.SetParameterStringUNICODE("Strip[2].Label", "TEST😊");
-            while (true){ 
-                remote.IsParametersDirty();
-                Console.WriteLine(remote.GetLevel(VoicemeeterLevelType.OUTPUT, VoicemeeterChannels.VOICEMEETER_POATATO_OUTPUT_A1_01));
-                System.Threading.Thread.Sleep(10);
+            var listener = new UDPListener(8000);
+            var sender = new UDPSender("192.168.2.31", 9000);
+
+            VoicemeeterType vmType = remote.GetVMType();
+            Console.WriteLine("Successfully connect to " + vmType + " version: " + remote.GetVMVersion());
+
+            while (true) {
+                try {
+                    OscMessage messageReceived = (OscMessage)listener.Receive();
+                    if (messageReceived != null) {
+                        // /vm/type/channel/cmd
+                        if (messageReceived.Address.StartsWith("/vm/")) {
+                            String[] d = messageReceived.Address.Split('/');
+                            if(d[2] == "strip" || d[2] == "bus") {
+                                switch (d[4]) {
+                                    case "gain":
+                                        remote.SetParameterFloat(d[2]+"[" + d[3] + "].Gain", (float)messageReceived.Arguments[0]);
+                                        break;
+                                    case "mute":
+                                        remote.SetParameterFloat(d[2]+"[" + d[3] + "].Mute", (float)messageReceived.Arguments[0]);
+                                        break;
+                                }
+                            }
+                        }
+                        Console.WriteLine(messageReceived.Address + " = " + messageReceived.Arguments[0].ToString());
+                    }
+                } catch (Exception e) { Console.WriteLine("Unknown exception occured: " + e.Message); }
+
+                if (vmType.ToString() == VoicemeeterType.VOICEMEETER.ToString()) {
+                }
+                if (vmType.ToString() == VoicemeeterType.VOICEMEETER_BANANA.ToString()) {
+                }
+                if (vmType.ToString() == VoicemeeterType.VOICEMEETER_POATATO.ToString()) {
+                }
+
+                for (int i = 0; i < (vmType.PhysicalCount + vmType.VirtualCount); i++) {
+                    remote.IsParametersDirty();
+                    sender.Send(new SharpOSC.OscMessage("/vm/bus/" + i.ToString() + "/gain", remote.GetParameterFloat("Bus[" + i.ToString() + "].Gain")));
+                    sender.Send(new SharpOSC.OscMessage("/vm/bus/" + i.ToString() + "/mute", remote.GetParameterFloat("Bus[" + i.ToString() + "].Mute")));
+                    sender.Send(new SharpOSC.OscMessage("/vm/bus/" + i.ToString() + "/level", 1, remote.GetLevel(VoicemeeterLevelType.OUTPUT, (VoicemeeterChannels)(i * vmType.PhysicalOutputChannelCount))));
+                    sender.Send(new SharpOSC.OscMessage("/vm/bus/" + i.ToString() + "/level", 2, remote.GetLevel(VoicemeeterLevelType.OUTPUT, (VoicemeeterChannels)(i * vmType.PhysicalOutputChannelCount + 1))));
+
+                    remote.IsParametersDirty();
+                    sender.Send(new SharpOSC.OscMessage("/vm/strip/" + i.ToString() + "/gain", remote.GetParameterFloat("Strip[" + i.ToString() + "].Gain")));
+                    sender.Send(new SharpOSC.OscMessage("/vm/strip/" + i.ToString() + "/mute", remote.GetParameterFloat("Strip[" + i.ToString() + "].Mute")));
+                    sender.Send(new SharpOSC.OscMessage("/vm/strip/" + i.ToString() + "/label", stringOR(remote.GetParameterStringUNICODE("Strip[" + i.ToString() + "].Label"), "A"+(i+1).ToString())));
+                    sender.Send(new SharpOSC.OscMessage("/vm/strip/" + i.ToString() + "/level", 1, remote.GetLevel(VoicemeeterLevelType.INPUT_POST_MUTE, (VoicemeeterChannels)(i < vmType.PhysicalCount ? (i * vmType.PhysicalInputChannelCount + 0) : (vmType.PhysicalCount * 2 + (i - vmType.PhysicalCount) * vmType.VirtualChannelCount + 0)))));
+                    sender.Send(new SharpOSC.OscMessage("/vm/strip/" + i.ToString() + "/level", 2, remote.GetLevel(VoicemeeterLevelType.INPUT_POST_MUTE, (VoicemeeterChannels)(i < vmType.PhysicalCount ? (i * vmType.PhysicalInputChannelCount + 1) : (vmType.PhysicalCount * 2 + (i - vmType.PhysicalCount) * vmType.VirtualChannelCount + 1)))));
+                }
             }
 
-           
-            //remote.Logout();
 
+            //remote.Logout();
             //Console.ReadLine();
         }
 	}
